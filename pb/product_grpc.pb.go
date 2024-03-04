3fbc1b5ff5649007a92638bca274735f35a46efd
Akshay Babu <akshaybabut1@gmail.com>
2024-03-04 10:32:36 +0530

    Update stock set

--- pb/product_grpc.pb.go
+++ pb/product_grpc.pb.go
@@ -22,6 +22,7 @@ const (
 	ProductService_AddProduct_FullMethodName    = "/product.ProductService/AddProduct"
 	ProductService_GetProduct_FullMethodName    = "/product.ProductService/GetProduct"
 	ProductService_GetAllProduct_FullMethodName = "/product.ProductService/GetAllProduct"
+	ProductService_UpdateStock_FullMethodName   = "/product.ProductService/UpdateStock"
 )
 
 // ProductServiceClient is the client API for ProductService service.
@@ -31,6 +32,7 @@ type ProductServiceClient interface {
 	AddProduct(ctx context.Context, in *AddProductRequest, opts ...grpc.CallOption) (*ProductResponse, error)
 	GetProduct(ctx context.Context, in *GetProductByID, opts ...grpc.CallOption) (*ProductResponse, error)
 	GetAllProduct(ctx context.Context, in *NoParam, opts ...grpc.CallOption) (ProductService_GetAllProductClient, error)
+	UpdateStock(ctx context.Context, in *UpdateStockRequest, opts ...grpc.CallOption) (*ProductResponse, error)
 }
 
 type productServiceClient struct {
@@ -91,6 +93,15 @@ func (x *productServiceGetAllProductClient) Recv() (*ProductResponse, error) {
 	return m, nil
 }
 
+func (c *productServiceClient) UpdateStock(ctx context.Context, in *UpdateStockRequest, opts ...grpc.CallOption) (*ProductResponse, error) {
+	out := new(ProductResponse)
+	err := c.cc.Invoke(ctx, ProductService_UpdateStock_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ProductServiceServer is the server API for ProductService service.
 // All implementations must embed UnimplementedProductServiceServer
 // for forward compatibility
@@ -98,6 +109,7 @@ type ProductServiceServer interface {
 	AddProduct(context.Context, *AddProductRequest) (*ProductResponse, error)
 	GetProduct(context.Context, *GetProductByID) (*ProductResponse, error)
 	GetAllProduct(*NoParam, ProductService_GetAllProductServer) error
+	UpdateStock(context.Context, *UpdateStockRequest) (*ProductResponse, error)
 	mustEmbedUnimplementedProductServiceServer()
 }
 
@@ -113,6 +125,9 @@ func (UnimplementedProductServiceServer) GetProduct(context.Context, *GetProduct
 }
 func (UnimplementedProductServiceServer) GetAllProduct(*NoParam, ProductService_GetAllProductServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetAllProduct not implemented")
+}
+func (UnimplementedProductServiceServer) UpdateStock(context.Context, *UpdateStockRequest) (*ProductResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateStock not implemented")
 }
 func (UnimplementedProductServiceServer) mustEmbedUnimplementedProductServiceServer() {}
 
@@ -184,6 +199,24 @@ func (x *productServiceGetAllProductServer) Send(m *ProductResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _ProductService_UpdateStock_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateStockRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).UpdateStock(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_UpdateStock_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).UpdateStock(ctx, req.(*UpdateStockRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ProductService_ServiceDesc is the grpc.ServiceDesc for ProductService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -198,6 +231,10 @@ var ProductService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetProduct",
 			Handler:    _ProductService_GetProduct_Handler,
+		},
+		{
+			MethodName: "UpdateStock",
+			Handler:    _ProductService_UpdateStock_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
